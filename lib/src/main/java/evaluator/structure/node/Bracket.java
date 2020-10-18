0e80512d65524fd16057eb8566c1bef8943c445a
andrea321123 <gal.andrea.van@gmail.com>
2020-10-18 14:34:44 +0200

    Add Bracket class

---
+++ lib/src/main/java/evaluator/structure/node/Bracket.java
@@ -0,0 +1,11 @@
+package evaluator.structure.node;
+
+/**
+ * Represent a node containing a bracket.
+ * Bracket can't return any value. Therefore if getValue is called
+ * on a Bracket object, it will return an exception.
+ * @author Andrea
+ * @version 1.0
+ */
+public class Bracket {
+}
---
+++ lib/src/main/java/evaluator/structure/node/NoValueException.java
@@ -0,0 +1,10 @@
+package evaluator.structure.node;
+
+/**
+ * Exception thrown when a node can't return a value.
+ * Bracket nodes can't return a value.
+ * @author Andrea
+ * @version 1.0
+ */
+public class NoValueException extends RuntimeException {
+}
