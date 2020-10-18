cf68b4818337766a8be79e4ecc5a5887b0499a04
andrea321123 <gal.andrea.van@gmail.com>
2020-10-18 17:50:01 +0200

    Update LexerList.java

--- lib/src/main/java/evaluator/structure/lexer/LexerList.java
+++ lib/src/main/java/evaluator/structure/lexer/LexerList.java
@@ -1,9 +1,42 @@
 package evaluator.structure.lexer;
 
+import evaluator.structure.node.Node;
+
+import java.util.LinkedList;
+
 /**
  * Converts an input string into a list of Nodes
  * @author Andrea
- * @version 1.2
+ * @version 1.3
  */
 public class LexerList {
+    public LinkedList<Node> list;
+    public String inputString;
+
+    LexerList(String string) {
+        inputString = string;
+        doLexing(string);
+    }
+    LexerList() {
+        inputString = "";
+    }
+    // LexerList doesn't have a copy constructor because we use only one LexerList object
+
+    // public methods
+    void doLexing(String inputString) {
+        list = new LinkedList<>();
+
+        // TODO: implement doLexing method
+
+        return;
+    }
+
+    // private methods
+    // creates a list of symbols from a string (symbols: 34, (, sin, +, ^, sqrt)
+    private LinkedList <String> split(String inputString) {
+
+        // TODO: implement split method
+
+        return new LinkedList<String>();
+    }
 }
