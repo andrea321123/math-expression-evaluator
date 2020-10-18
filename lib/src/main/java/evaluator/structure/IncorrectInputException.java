aa2e06e46160f45ae4840547c9551958d3570a15
andrea321123 <gal.andrea.van@gmail.com>
2020-10-18 19:37:11 +0200

    Update split() method and IncorrectInputException

---
+++ lib/src/main/java/evaluator/structure/IncorrectInputException.java
@@ -0,0 +1,9 @@
+package evaluator.structure;
+
+/**
+ * Exception thrown when the input isn't correct
+ * @author Andrea
+ * @version 1.0
+ */
+public class IncorrectInputException extends RuntimeException {
+}
--- lib/src/main/java/evaluator/structure/lexer/LexerList.java
+++ lib/src/main/java/evaluator/structure/lexer/LexerList.java
@@ -1,5 +1,7 @@
 package evaluator.structure.lexer;
 
+import evaluator.structure.IncorrectInputException;
+
 import evaluator.structure.node.Node;
 
 import java.util.LinkedList;
@@ -7,11 +9,12 @@ import java.util.LinkedList;
 /**
  * Converts an input string into a list of Nodes
  * @author Andrea
- * @version 1.3
+ * @version 1.4
  */
 public class LexerList {
     public LinkedList<Node> list;
     public String inputString;
+    public StringToEnum stringToEnum = new StringToEnum();
 
     LexerList(String string) {
         inputString = string;
@@ -34,9 +37,63 @@ public class LexerList {
     // private methods
     // creates a list of symbols from a string (symbols: 34, (, sin, +, ^, sqrt)
     private LinkedList <String> split(String inputString) {
+        LinkedList <String> returnList = new LinkedList<>();
 
-        // TODO: implement split method
+        boolean finishedSymbol = true;  // flag if on the last iteration we inserted a symbol in the list
+        String currentString = "";      // current string we are evaluating
+        // we read each single char of the input string
+        for (int i = 0; i < inputString.length(); i++) {
+            if (finishedSymbol) {
+                currentString = Character.toString(inputString.charAt(i));
 
-        return new LinkedList<String>();
+                if (stringToEnum.get(currentString) != null)
+                    returnList.add(currentString);
+                else {
+                    // we must check if it is the beginning of a number
+                    if (isNumber(currentString)) {
+                        while (isNumber(currentString)) {
+                            try {
+                                String newChar = Character.toString(inputString.charAt(++i));
+                                currentString += newChar;
+                            }
+                            catch (IndexOutOfBoundsException e) {       // we reached end of string
+                                returnList.add(currentString);
+                                break;
+                            }
+                        }
+                        // we have to remove last character (because it isn't a number)
+                        currentString = currentString.substring(0, currentString.length() -1);
+                        i--;
+                    }
+                    else
+                        finishedSymbol = false;
+                }
+            }
+            else {          // symbol isn't finished yet
+                currentString += Character.toString(inputString.charAt(i));
+
+                if (stringToEnum.get(currentString) != null) {
+                    returnList.add(currentString);
+                    finishedSymbol = true;
+                }
+            }
+        }
+
+        // we have to check if the last symbol was a correct symbol. In this case, the input format is correct
+        if (!finishedSymbol)
+            throw new IncorrectInputException();
+
+        return returnList;
+    }
+
+    private boolean isNumber(String input) {
+        try {
+            double castedNumber = Double.parseDouble(input);
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
     }
 }
