a2311cfa6b367d91091da2ddc43016a2f3db7e0a
andrea321123 <gal.andrea.van@gmail.com>
2020-10-24 22:37:44 +0200

    Create InputErrorException class

---
+++ lib/src/main/java/evaluator/InputErrorException.java
@@ -0,0 +1,9 @@
+package evaluator;
+
+/**
+ * This exception is thrown when the input for the evaluator is not correct (es: "3+(4+)")
+ * @author Andrea
+ * @version 1.0
+ */
+public class InputErrorException extends RuntimeException {
+}
